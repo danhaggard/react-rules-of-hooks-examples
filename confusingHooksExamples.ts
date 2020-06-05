08752b62295c9fc2307c2932b61b8a13e2350978
Dan Haggard <danhaggard10@gmail.com>
2020-06-06 01:09:08 +1000

    Update confusingHooksExamples.ts

--- confusingHooksExamples.ts
+++ confusingHooksExamples.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 
 /*
-  higherOrderFunc and impureFunc - wiil be used to illustrate
+  higherOrderFunc and impureHoF - wiil be used to illustrate
   some issues in the hooks below.
 */
 export const higherOrderFunc = (arg: number): any => {
@@ -14,8 +14,9 @@ export const higherOrderFunc = (arg: number): any => {
 let mutatedVal = 10;
 setTimeout(() => (mutatedVal = 20), 5000);
 
-export const impureFunc = (arg: number): any => {
-  return mutatedVal + arg;
+export const impureHoF = (arg: number): any => {
+  const impureFunc = (): number => mutatedVal + arg;
+  return impureFunc;
 };
 
 export const useConfusingHooksExample = (): Record<any, Function> => {
@@ -31,13 +32,26 @@ export const useConfusingHooksExample = (): Record<any, Function> => {
     unMemoized will always return the same value.
 
     Also - memoized ultimately relies on an external namespace as well which
-    the linter otherwise warns you about because of the fear of mutations
-    in external namespaces not triggering re-renders - but here it doesn't
+    the linter otherwise warns you about because of the fear that mutations
+    in external namespaces won't trigger re-renders immediately - causing
+    side effects in application behaviour that are more difficult to understand.
+    
+    But then ultimately any namespace - even if declared in scope - could depend on another
+    which is impure or mutates (like impureFunc) that the linter doesn't check. e.g.
+  
+    const unMemoized = impureHoF(5);
+   
+    const memoized = useCallback(() => {
+      return unMemoized();
+    }, [unMemoized]);
 
+    If dependency on external namespaces is bad because of mutation - should we care in this
+    case too?
   */
   const memoized = useCallback(() => {
     return unMemoized();
   }, [unMemoized]);
+
 
   /*
     Fail?
@@ -55,13 +69,14 @@ export const useConfusingHooksExample = (): Record<any, Function> => {
     This passes even though it is functionally equivalent to
     memoizedTwo and correctly memoises.
 
-    But note it has a direct reference to an external namespace.
-
-    Normally the linter would reject this I asumme because values in external
-    names spaces could have mutations that won't trigger rerenders -
-    the mutations showing up in the application in ways that are difficult
-    to follow.
-
+    But note it has a direct reference to an external namespace that it would normally reject
+    because of the external namespace concern.  Seems to be an eslint bug caused
+    because the value of the first arg passed to useCallback isn't a function
+    declared inline.
+    
+    It doesn't care if :
+    useCallback(higherOrderFunc(5), []); or
+    useCallback(impureHoF(5), [impureHoF]);
   */
   const memoizedThree = useCallback(higherOrderFunc(5), [higherOrderFunc]);
 
